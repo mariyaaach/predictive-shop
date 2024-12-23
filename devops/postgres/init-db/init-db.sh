3899a9fdbf00f52558d4d3ea79a9d569ecf4bb03
Egor Medvedev <egor-medvedev00@mail.ru>
2024-12-23 21:51:02 +0300

    добавил базу под продукты и товары

--- devops/postgres/init-db/init-db.sh
+++ devops/postgres/init-db/init-db.sh
@@ -5,4 +5,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-EOSQL
     CREATE DATABASE users_service;
     CREATE DATABASE orders_service;
+    CREATE DATABASE products_service;
 EOSQL
